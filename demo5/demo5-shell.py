c11236ff360a8c7d777e3471bbfafea3cc374c5d
bigchui <146177721+Nowake-stack@users.noreply.github.com>
2026-09-09 20:05:08 +0800

    Add files via upload

---
+++ demo5/demo5-shell.py
@@ -0,0 +1,44 @@
+Python 3.14.7 (tags/v3.14.7:823f032, Aug  5 2026, 10:51:32) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+3/2
+1.5
+3//2
+1
+>>> -3/2
+-1.5
+>>> -3//2
+-2
+>>> 3%2
+1
+>>> 6%2
+0
+>>> divmod(3,2)
+(1, 1)
+>>> divmod(-3,2)
+(-2, 1)
+>>> z = 1 +  2j
+>>> abs(z)
+2.23606797749979
+>>> int(3.14)
+3
+>>> int(3.99)
+3
+>>> float("3.14")
+3.14
+>>> float("520")
+520.0
+>>> float('+1e6')
+1000000.0
+>>> complex("1+2j")
+(1+2j)
+>>> complex("1 + 2j")
+Traceback (most recent call last):
+  File "<pyshell#16>", line 1, in <module>
+    complex("1 + 2j")
+ValueError: complex() arg is a malformed string
+>>> pow(2,3)
+8
+>>> 2**3
+8
+>>> pow(2,3,5)
+3
---
+++ demo5/demo5.py
@@ -0,0 +1,17 @@
+
+# i = 0
+# while i < 1:
+#     i = i + 0.1
+#     print(i)
+
+# import decimal  #十进制模块,用于精确处理浮点数
+# a = decimal.Decimal('0.1')
+# b = decimal.Decimal('0.2')
+# c = decimal.Decimal('0.3')
+# if a + b == c:
+#     print('a + b 等于 c')
+
+# x = 1 + 2j
+# print(x.real)  # 输出实部
+# print(x.imag)  # 输出虚部
+
